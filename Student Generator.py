ce440da7c0c8084943dba05079a239fcfb70834e
JWitterick <jpwitterick@gmail.com>
2021-06-25 09:53:50 +1000

    Student class definition

--- Student Generator.py
+++ Student Generator.py
@@ -1,1 +1,12 @@
 # Kyan paste the student generator code here
+class Student:
+    def __init__(self, number, vce, p1, p2, p3, p4, b1, b2):
+        self.number = number
+        self.vce = vce
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.p4 = p4
+        self.b1 = b1
+        self.b2 = b2
+
